f8bd084e337b9083f0e8a6f5af95f6b9a7958f4c
arnoldas500 <akurbanovas@albany.edu>
2017-04-25 00:48:28 -0400

    working test of execute method
    only run execute method or comment out next method in program.cs to see it working

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+    class Program
+    {
+        //creating the reference for our browser
+        IWebDriver driver = new ChromeDriver();
+
+        static void Main(string[] args)
+        {
+            
+
+        }
+
+        [SetUp]
+        public void Initilize()
+        {
+            /*
+            //Navigate to google page
+            driver.Navigate().GoToUrl("http://www.google.com");
+            Console.WriteLine("Opened URL");
+            */
+
+            //Navigate to google page
+            driver.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html?UserName=&amp;Password=&amp;Login=Login﻿");
+            Console.WriteLine("Opened URL");
+
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+
+            //selecting the title
+            //going to call the custom method that i made
+            SeleniumSetMethods.SelectDropDown(driver, "TitleId", "Mr.", "Id");
+
+            //initial
+            SeleniumSetMethods.EnterText(driver, "Initial", "executeautomation", "Name");
+
+            //The click button
+            SeleniumSetMethods.Click(driver, "Save", "Name");
+
+
+            /*
+            //EnterText(element, value, type)
+
+            //to find this you go to chrome, inspect elements after rigth clicking
+            //and then find the text box and the name for the text box which is "q"
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            //What will actually get send to the google search text box
+            element.SendKeys("This is the shit!!!");
+
+            Console.WriteLine("Executed test");
+        */
+        }
+
+        [Test]
+        public void NextTest()
+        {
+            Console.WriteLine("This is the next test");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            //closes the browser for you after its done with everything
+            driver.Close();
+            Console.WriteLine("Closed the browser");
+        }
+
+    }
+}
---
+++ SeleniumSetMethods.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium
+{
+    class SeleniumSetMethods
+    {
+
+        //enter text
+        public static void EnterText(IWebDriver driver, string element, string value, string elementType)
+        {
+            if(elementType == "Id")
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            if (elementType == "Name")
+                driver.FindElement(By.Name(element)).SendKeys(value);
+        }
+
+        //click operation inro a button, checkbox, option, etc
+        public static void Click(IWebDriver driver, string element, string elementType)
+        {
+            if (elementType == "Id")
+                driver.FindElement(By.Id(element)).Click();
+            if (elementType == "Name")
+                driver.FindElement(By.Name(element)).Click();
+        }
+
+        //method for selecting a drop down control
+        public static void SelectDropDown(IWebDriver driver, string element, string value, string elementType)
+        {
+            //SelectElement selectElement = new SelectElement();
+            if (elementType == "Id")
+                new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+            if (elementType == "Name")
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+        }
+
+
+    }
+}
